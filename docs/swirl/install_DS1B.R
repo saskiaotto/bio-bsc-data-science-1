2e2460d02e0127b7bcdeac03c3f47a6382971927
Otto, Dr. Saskia <saskia.otto@uni-hamburg.de>
2021-01-06 12:25:23 +0100

    new import functions that will hopefully solve lesson issues

--- docs/swirl/install_DS1B.R
+++ docs/swirl/install_DS1B.R
@@ -1,26 +1,10 @@
-install_DS1 <- function(){
-  writeLines(strwrap("Jetzt werden die swirl-Kursdateien fuer das erste DS Modul runtergeladen. Diese Dateien mit der Endung .swc sind fuer die Nutzung der Kurse nicht erforderlich, Du kannst sie nach der Installation loeschen. \n\nEs hat sich ein neues Fenster geoeffnet.  Waehle dort einen beliebigen Speicherort fuer die Dateien. (Das Fenster oeffnet sich evtl. hinter Deinem RStudio-Fenster)"));
-  choose_directory = function(caption = 'Select data directory') {
-    if (exists('utils::choose.dir')) {
-      choose.dir(caption = caption)
-    } else {
-      library("tcltk")
-      tk_choose.dir(caption = caption)
-    }
-  };
-  target_folder <- choose_directory();
-  course_list <- c("UHH-DSB-Data_Science_1B");
-  for(i in seq_along(course_list)){
-    download.file(url = paste0("https://saskiaotto.github.io/bio-bsc-data-science-1/swirl/", course_list[i], ".swc"), destfile = paste0(target_folder, "/", course_list[i], ".swc"), mode = "wb")
-  };
-  if(!length(unique((installed.packages()[,1] == "swirl"))) == 2){install.packages("swirl")};
-  library(swirl);
-  for(i in seq_along(course_list)){
-    install_course(swc_path = paste0(target_folder, "/", course_list[i], ".swc"))
-  };
-  swirl(select_language(language = "german"))
+install_dsb_course <- function(){
+
+    install_course_url("https://saskiaotto.github.io/bio-bsc-data-science-1/swirl/UHH-DSB-Data_Science_1B.swc.zip")
+    path <- file.path(swirl_courses_dir(), "UHH-DSB-Data_Science_1B.swc")
+    install_course(swc_path = path)
+    unlink(path)
 }
 
-install_DS1()
-
-rm(install_DS1)
+install_dsb_course()
+rm(install_dsb_course)
