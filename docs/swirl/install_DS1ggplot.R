cc73a2ba0855e65fb4b7e2be41e6c7aad3238d9f
Otto, Dr. Saskia <saskia.otto@uni-hamburg.de>
2021-01-25 11:16:36 +0100

    added Rlab ggplot course

---
+++ docs/swirl/install_DS1ggplot.R
@@ -0,0 +1,16 @@
+install_dsb_course <- function(){
+
+  swirl::install_course_url("https://saskiaotto.github.io/bio-bsc-data-science-1/swirl/daten-vis-ggplot2.swc.zip")
+  get_course_path <- function(){
+    tryCatch(swirl:::swirl_courses_dir(),
+             error = function(c) {file.path(find.package("swirl"),"Courses")}
+    )
+  }
+  path <- file.path(get_course_path(), "daten-vis-ggplot2.swc.swc")
+  swirl::install_course(swc_path = path)
+  unlink(path)
+
+}
+
+install_dsb_course()
+rm(install_dsb_course)
